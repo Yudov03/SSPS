5d594ece338351522b6eea229ec8c4ae2cacbe04
Duy <volydacduy942003@gmail.com>
2024-12-05 19:19:06 +0700

    update app

--- Frontend/SSPS/src/App.jsx
+++ Frontend/SSPS/src/App.jsx
@@ -12,6 +12,11 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import Login from "./component/login";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import Dashboard from "./component/Dashboard/dashboard"
+import Student from "./component/student"
+import Config from "./component/config"
+import Help from "./component/help";
+import Setting from "./component/setting"
 
 function App() {
   const location = useLocation()
@@ -31,6 +36,11 @@ function App() {
           <Route path="/printers/add" element={<AddPrinter />} />
           <Route path="/printers/edit/:id" element={<EditPrinter />} />
           <Route path="/printers/info/:id" element={<DetailPrinter />} />
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/student" element={<Student/>}/>
+          <Route path="/config" element={<Config/>}/>
+          <Route path="/help" element={<Help/>}/>
+          <Route path="/setting" element={<Setting/>}/>
         </Routes>
       }
     />
